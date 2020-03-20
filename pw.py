97a0834e4420dd607e115d14dc2229208ce6ee2e
gridley-c <crgridley@gmail.com>
2020-03-20 14:29:30 +0000

    Added account argument

--- pw.py
+++ pw.py
@@ -5,4 +5,11 @@ PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
              'blog': 'VmALvQyKAxiVH5G8vo1if1MLZF3sdt',
              'luggage': '12345'}
 
+import sys
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1] # first command line arg is the account name
+
 
